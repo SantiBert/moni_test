7a02cc7f46c37c1c9719fea708dec9e9e7772fe5
Santiago Bertero <50256319+SantiBert@users.noreply.github.com>
2021-05-06 01:11:40 -0300

    Update prod_settings.py

--- moni_test/settings/prod_settings.py
+++ moni_test/settings/prod_settings.py
@@ -1,18 +1,16 @@
 from .base import *
 """
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+ALLOWED_HOSTS = ['monitestsantiagobertero.pythonanywhere.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'monitestsantiago$default',
+        'USER': 'monitestsantiago',
+        'PASSWORD': 'pruebatecnica13',
+        'HOST': 'monitestsantiagobertero.mysql.pythonanywhere-services.com',
     }
 }
 """
